4a26ba668b482d015bf73dd4e396a9bf8f4bb012
lupindevv <alexxthm1@gmail.com>
2024-04-02 20:05:02 +0300

    fixed UI test

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
     await page.goto('http://localhost:8080/shopping-list');
-    const list = await page.$('dl');
+    const list = await page.$('a[href="/Shopping-List"]');
     expect(list).toBeTruthy();
   });
   
